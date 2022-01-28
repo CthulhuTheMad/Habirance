09889a53854ea07195bc506541c9aac730d5007d
ABlackPikatchu <J.Wimbrough@gmail.com>
2022-01-27 22:15:40 -0500

    --Habirance Ver-0.0.1-Alpha Hotfix-1--
    **FIXES**
    -Missing loot tables for limestone have been added
    -Fixed a model error for limestone_block_item
    
    **CHANGES**
    -Alabaster now spawns starting at Y level -15 with less veins per chunk
    -Redrock now spawns less frequently in a chunk

--- src/main/java/net/endurteam/habirance/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/endurteam/habirance/world/feature/ModPlacedFeatures.java
@@ -9,11 +9,11 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class ModPlacedFeatures {
     public static final PlacedFeature ORE_ALABASTER_PLACED = PlacementUtils.register("ore_alabaster_placed",
-            ModConfiguredFeature.ORE_ALABASTER.placed(ModOrePlacement.commonOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(0)))));
+            ModConfiguredFeature.ORE_ALABASTER.placed(ModOrePlacement.commonOrePlacement(1, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(-15)))));
     public static final PlacedFeature ORE_REDROCK_UPPER_PLACED = PlacementUtils.register("ore_redorck_upper_placed",
-            ModConfiguredFeature.ORE_REDROCK.placed(ModOrePlacement.rareOrePlacement(6, HeightRangePlacement.uniform(VerticalAnchor.absolute(64), VerticalAnchor.absolute(128)))));
+            ModConfiguredFeature.ORE_REDROCK.placed(ModOrePlacement.rareOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.absolute(64), VerticalAnchor.absolute(128)))));
     public static final PlacedFeature ORE_REDROCK_LOWER_PLACED = PlacementUtils.register("ore_redorck_lower_placed",
-            ModConfiguredFeature.ORE_REDROCK.placed(ModOrePlacement.commonOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(60)))));
+            ModConfiguredFeature.ORE_REDROCK.placed(ModOrePlacement.commonOrePlacement(2, HeightRangePlacement.uniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(60)))));
     public static final PlacedFeature ORE_LIMESTONE_UPPER_PLACED = PlacementUtils.register("ore_limestone_upper_placed",
             ModConfiguredFeature.ORE_LIMESTONE.placed(ModOrePlacement.rareOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.absolute(64), VerticalAnchor.absolute(128)))));
     public static final PlacedFeature ORE_LIMESTONE_LOWER_PLACED = PlacementUtils.register("ore_limestone_lower_placed",
